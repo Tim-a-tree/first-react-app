fdca65d3dfa2137accee5102996cc31ad0d2d884
JeongHyunHeo <21700786@handong.edu>
2022-07-11 11:14:01 +0900

    first component created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return <header>
+    <h1><a href="/index.html">WEB</a></h1>
+  </header>
+}
+
+function Article()
+{
+  return <article>
+    <h2>Welcome, WEB</h2>
+  </article>
+}
+
+function Nav()
+{
+  return <nav>
+    <ol>
+      <li><a href="read/1.html">HTML</a></li>
+      <li><a href="read/2.html">CSS</a></li>
+      <li><a href="read/3.html">JavaScript</a></li>
+    </ol>
+  </nav>
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
