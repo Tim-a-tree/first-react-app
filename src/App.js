b04a685dfe4f628cf4750f0b9931c1c9d54613bc
JeongHyunHeo <21700786@handong.edu>
2022-07-11 14:59:21 +0900

    merging

--- src/App.js
+++ src/App.js
@@ -7,31 +7,61 @@ function Header() {
   </header>
 }
 
-function Article()
+function Article(props)
 {
+  console.log('props', props);
   return <article>
-    <h2>Welcome, WEB!!!!</h2>
+    <h2>{props.title}</h2>
+    <p>{props.body}</p>
   </article>
 }
 
-function Nav()
+function Nav(props)
 {
+  // const tag = []
+  // for (let i = 0; i < props.data.length; i++)
+  // {
+  //   tag.push(<li key={props.data[i].id}><a href={'/read' + props.data[i].id}>{props.data[i].title}</a></li>);
+  // }
+
+
+  // function callback(element) {
+  //   return <li key={element.id}>
+  //     <a href={'/read/' + element.id}>
+  //       {element.title}
+  //     </a>
+  //   </li>
+  // }
+
+
+  const callback = (el) => {
+    return <li key={el.id}>
+      <a href={'/read/' + el.id}>
+        {el.title}
+      </a>
+    </li>
+  }
+  const tag = props.data.map(callback);
   return <nav>
     <ol>
-      <li><a href="read/1.html">HTML</a></li>
-      <li><a href="read/2.html">CSS</a></li>
-      <li><a href="read/3.html">JavaScript</a></li>
+      {tag}
     </ol>
   </nav>
 }
 
 
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ....' },
+    { id: 2, title: 'css', body: 'css is ....' },
+    { id: 3, title: 'javascript', body: 'javascript is ....' }
+  ]
   return (
     <div className="App">
       <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Nav data={topics}></Nav>
+      <Article title="HTML" body="Hello, WEB"></Article>
+      <Article title="CSS" body="Hello, WEB-CSS"></Article>
     </div>
   );
 }
