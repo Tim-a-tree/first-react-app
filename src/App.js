1e99ec2b9b5de7cd376a98032b7eafe8850924c9
JeongHyunHeo <21700786@handong.edu>
2022-07-13 14:38:37 +0900

    form with auto increment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import { Link, Route, Routes, useParams } from 'react-router-dom';
+import { Link, Route, Routes, useParams, useNavigate } from 'react-router-dom';
 
 function Header(props){
   return <header>
@@ -39,26 +39,34 @@ function Web()
   return <Article title="Welcome" body="Hello, WEB"></Article>
 }
 
-function Create()
+function Create(props)
 {
   return (
     <article body="Welcome to creation page">
       <h1>Create</h1>
-      <form action="/api/create">
+      <form action="/api/create" onSubmit={event => {
+        event.preventDefault();
+        var title = event.target.title.value;
+        var body = event.target.body.value;
+        props.onCreate(title, body);
+      }}>
         <p><input type="text" name="title" placeholder="제목"/></p>
         <p><textarea name="body" placeholder="본문"></textarea></p>
         <p><input type="submit" value="생성"></input></p>
+        
       </form>
   </article>
   ); 
 }
 
 function App() {
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'js', body:'js is ...'}
-  ]
+  ])
+  const navigate = useNavigate();
   return (
     <div style={{border: '1px solid red'}}>
         <Header></Header>
@@ -67,7 +75,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Web></Web>}></Route>
         <Route path="/read/:id" element={<Read data={topics}></Read>}></Route>
-        <Route path="/create" element={<Create></Create>}></Route>
+        <Route path="/create" element={<Create onCreate={(title, body) => {
+          const newTopics = [...topics];
+          newTopics.push({ id: nextId, title: title, body: body });
+          setTopics(newTopics);
+          navigate('/read/' + nextId);
+          setNextId(nextId + 1);
+        }}></Create>}></Route>
       </Routes>
       <Link to="/create">create</Link>
     </div>
