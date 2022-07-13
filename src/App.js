4b4a5c1475f9b9b26f26343a4845ab1d2e534b7e
JeongHyunHeo <21700786@handong.edu>
2022-07-13 13:47:15 +0900

    form added

--- src/App.js
+++ src/App.js
@@ -1,69 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Link, Route, Routes, useParams } from 'react-router-dom';
 
-function Header() {
+function Header(props){
   return <header>
-    <h1><a href="/index.html">WEB - Login</a></h1>
+    <h1><Link to="/" >WEB</Link></h1>
   </header>
 }
-
-function Article(props)
-{
-  console.log('props', props);
-  return <article>
-    <h2>{props.title}</h2>
-    <p>{props.body}</p>
-  </article>
-}
-
-function Nav(props)
-{
-  // const tag = []
-  // for (let i = 0; i < props.data.length; i++)
-  // {
-  //   tag.push(<li key={props.data[i].id}><a href={'/read' + props.data[i].id}>{props.data[i].title}</a></li>);
-  // }
-
-
-  // function callback(element) {
-  //   return <li key={element.id}>
-  //     <a href={'/read/' + element.id}>
-  //       {element.title}
-  //     </a>
-  //   </li>
-  // }
-
-
-  const callback = (el) => {
-    return <li key={el.id}>
-      <a href={'/read/' + el.id}>
-        {el.title}
-      </a>
-    </li>
-  }
-  const tag = props.data.map(callback);
+function Nav(props){
   return <nav>
     <ol>
-      {tag}
+      {props.data.map(el => <li key={el.id}>
+        <Link to={'/read/'+el.id}>
+          {el.title}
+        </Link>
+      </li>)}
     </ol>
   </nav>
 }
+function Article(props) {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>;
+}
 
+function Read(props)
+{
+  const params = useParams();
+  const id = Number(params.id);
+  const topic = props.data.filter(el => el.id === id)[0];
+  return <Article title={topic.title} body={topic.body} ></Article>
+}
+
+function Web()
+{
+  return <Article title="Welcome" body="Hello, WEB"></Article>
+}
+
+function Create()
+{
+  return (
+    <article body="Welcome to creation page">
+      <h1>Create</h1>
+      <form action="/api/create">
+        <p><input type="text" name="title" placeholder="제목"/></p>
+        <p><textarea name="body" placeholder="본문"></textarea></p>
+        <p><input type="submit" value="생성"></input></p>
+      </form>
+  </article>
+  ); 
+}
 
 function App() {
   const topics = [
-    { id: 1, title: 'html', body: 'html is ....' },
-    { id: 2, title: 'css', body: 'css is ....' },
-    { id: 3, title: 'javascript', body: 'javascript is ....' }
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'}
   ]
   return (
-    <div className="App">
-      <Header></Header>
-      <Nav data={topics}></Nav>
-      <Article title="HTML" body="Hello, WEB"></Article>
-      <Article title="CSS" body="Hello, WEB-CSS"></Article>
+    <div style={{border: '1px solid red'}}>
+        <Header></Header>
+        <Nav data={topics}></Nav>
+      {/* {content} */}
+      <Routes>
+        <Route path="/" element={<Web></Web>}></Route>
+        <Route path="/read/:id" element={<Read data={topics}></Read>}></Route>
+        <Route path="/create" element={<Create></Create>}></Route>
+      </Routes>
+      <Link to="/create">create</Link>
     </div>
   );
 }
 
 export default App;
+
